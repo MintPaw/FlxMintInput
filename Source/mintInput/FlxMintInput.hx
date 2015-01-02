74fe1970741218a1c96b17690f6608e7a5b1fcf6
MintPaw <jerusanders@gmail.com>
2015-01-01 19:50:17 -0700

    Initial commit

---
+++ Source/mintInput/FlxMintInput.hx
@@ -0,0 +1,21 @@
+package;
+
+import openfl.display.Stage;
+
+public class FlxMintInupt
+{
+	private var _stageRef:Stage;
+
+	public function new()
+	{
+
+	}
+
+	public static function init(stage:Stage):Void
+	{
+		_stageRef = stage;
+		
+		//_stageRef
+	}
+
+}
